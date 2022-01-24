9c5b664bd55896ce73cbad8126d6f7ff49065b80
DeanDodds <deandodds88@gmail.com>
2022-01-24 15:37:58 +0000

    feat: settings btn functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 const startPage = document.getElementById('start-page');
 const settingsPage = document.getElementById('settingspage');
 const RedBallPage = document.getElementById('red-ball-section');
+const gameBtns = document.getElementById('game-buttons');
 
 // Getting Buttons by id 
 const startBtn = document.getElementById('start-btn');
 const backBtn = document.getElementById('back-game-btn');
+const startGameBtn = document.getElementById('start-game-btn');
 
 startBtn.addEventListener('click', function () {
     startPage.classList.add('hidden');
@@ -16,3 +18,24 @@ backBtn.addEventListener('click', function () {
     startPage.classList.remove('hidden');
     settingsPage.classList.add('hidden');
 });
+
+startGameBtn.addEventListener('click', getGameSettings)
+
+function getGameSettings() {
+    event.preventDefault()
+    let numberOfFrames = document.getElementById('frames-input').value;
+    let playerOne = document.getElementById('player-one-input').value;
+    let playerTwo = document.getElementById('player-two-input').value;
+    document.getElementById('number-of-frames').innerHTML = numberOfFrames
+    document.getElementById('player-one-name').innerHTML = playerOne;
+    document.getElementById('player-two-name').innerHTML = playerTwo;
+    document.getElementById('score-board').classList.remove('hidden')
+    startGame()
+}
+
+function startGame() {
+    RedBallPage.classList.remove('hidden')
+    gameBtns.classList.remove('hidden')
+    settingsPage.classList.add('hidden');
+    document.getElementById('footer').classList.add('hidden');
+}
