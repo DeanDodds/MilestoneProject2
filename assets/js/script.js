9493b203cd54f2dcf07f1c102faa0d5de1b7183d
DeanDodds <deandodds88@gmail.com>
2022-01-26 22:41:07 +0000

    maint: extra comments added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,24 +20,29 @@ let currentPlayerScoreMarker = document.getElementById('player-one-score')
 let activePlayerOneMarker = document.getElementById('active-left');
 let activePlayerTWOMarker = document.getElementById('active-right');
 
-let points = 0;
-let breaktotal = 0;
+let points = 0; // points each ball is worth
+let breaktotal = 0; // total of current break
+let remainingPoints = 147; // remaining points on the table
 
 
 let playerScore = 0
 
+// start button event listener
 startBtn.addEventListener('click', function () {
     startPage.classList.add('hidden');
     settingsPage.classList.remove('hidden');
 });
 
+// back button event listener
 backBtn.addEventListener('click', function () {
     startPage.classList.remove('hidden');
     settingsPage.classList.add('hidden');
 });
 
+//start game button event listener
 startGameBtn.addEventListener('click', getGameSettings)
 
+// ball buton event listerners
 for (let ballBtn of ballBtns) {
     ballBtn.addEventListener('click', function () {
         points = parseInt(this.innerHTML)
@@ -50,12 +55,15 @@ for (let ballBtn of ballBtns) {
     });
 }
 
+// end break button event listener
 endBreakBtn.addEventListener('click', function () {
     breaktotal = 0
     console.log(breaktotal)
     switchPlayer()
     clearBreak()
 });
+
+
 
 /**
  * Gets the users input and displays it in the scoreboard element
@@ -84,7 +92,7 @@ function startGame() {
 
 /** 
  * change Display funtion changes between the red ball display and color ball display by adding and removing the hidden class.
-*/
+ */
 function changeDisplay(num) {
     if (num == 1) {
         RedBallPage.classList.add('hidden');
@@ -97,16 +105,16 @@ function changeDisplay(num) {
 /** 
  * The switch player fumction swaps the active triangle between the players and 
  * swaps the the scoring between the two players 
- * */ 
+ * */
 function switchPlayer() {
     changeDisplay()
-    if (activePlayerOneMarker.classList.contains('active')){
+    if (activePlayerOneMarker.classList.contains('active')) {
         alert('player tws turn');
         activePlayerOneMarker.classList.remove('active');
         activePlayerTWOMarker.classList.add('active');
         currentPlayerScoreMarker = document.getElementById('player-two-score');
         playerScore = parseInt(currentPlayerScoreMarker.innerHTML)
-    }else {
+    } else {
         alert('players one turn')
         activePlayerTWOMarker.classList.remove('active');
         activePlayerOneMarker.classList.add('active');
@@ -121,8 +129,8 @@ function switchPlayer() {
  * the funtion
  * adds one to the break counter of the breakball counter
  */
-function displayBreakBalls(num,){
-    let ballCounter; 
+function displayBreakBalls(num, ) {
+    let ballCounter;
     let num1
     if (num == 1) {
         alert('display red')
@@ -130,37 +138,37 @@ function displayBreakBalls(num,){
         ballCounter = parseInt(document.getElementById('red-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('red-break-counter').innerHTML = num1;
-    }else if (num == 2){
+    } else if (num == 2) {
         alert('display yellow')
         document.getElementById('yellow-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('yellow-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('yellow-break-counter').innerHTML = num1;
-    }else if (num == 3){
+    } else if (num == 3) {
         alert('display green')
         document.getElementById('green-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('green-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('green-break-counter').innerHTML = num1;
-    }else if (num == 4){
+    } else if (num == 4) {
         alert('display brown')
         document.getElementById('brown-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('brown-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('brown-break-counter').innerHTML = num1;
-    }else if (num == 5){
+    } else if (num == 5) {
         alert('display blue')
         document.getElementById('blue-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('blue-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('blue-break-counter').innerHTML = num1;
-    }else if (num == 6){
+    } else if (num == 6) {
         alert('display pink')
         document.getElementById('pink-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('pink-break-counter').innerHTML);
         num1 = ballCounter + 1;
         document.getElementById('pink-break-counter').innerHTML = num1;
-    }else if (num == 7){
+    } else if (num == 7) {
         alert('display black')
         document.getElementById('black-ball-break-counter').classList.remove('hidden');
         ballCounter = parseInt(document.getElementById('black-break-counter').innerHTML);
@@ -173,7 +181,7 @@ function displayBreakBalls(num,){
  * adds class of hidden to break counter elements
  * and resets counters to 0
  */
-function clearBreak(){
+function clearBreak() {
     let breakCounters = document.getElementsByClassName('small');
     let breakCounterInners = document.getElementsByClassName('break-ball-counter');
 
@@ -181,7 +189,11 @@ function clearBreak(){
         breakCounter.classList.add('hidden')
     }
 
-    for (let breakCounterInner of breakCounterInners){
+    for (let breakCounterInner of breakCounterInners) {
         breakCounterInner.innerHTML = 0;
     }
+}
+
+function trackRemainingPoints() {
+
 }
